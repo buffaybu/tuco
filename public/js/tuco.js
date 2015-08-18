57f8da153e404f6e1e3b9a5e39cf0269850f41b9
BuffayBu <BuffayBu@gmail.com>
2015-08-18 17:32:40 +0800

    Add user's custom avatar

--- public/js/tuco.js
+++ public/js/tuco.js
@@ -947,6 +947,79 @@ return Dragdealer;
 // imgAvatar, imgLogo: url of the image
 // rgba: an rgba string. e.g. 'rgba(255, 153, 153, .7)'
 (function() {
+  var avatarURL = '';
+  var $file = document.getElementById('file');
+  $file.onchange = function() {
+    var $avatar = document.getElementById('avatar');
+    var file = $file.files[0];
+    var reader = new FileReader();
+
+    reader.onloadend = function() {
+      avatarURL = reader.result;
+      document.getElementById('avatar').style.backgroundImage = 'url(' + avatarURL + ')';
+    }
+
+    if (file) {
+      reader.readAsDataURL(file);
+    }
+  }
+
+  /**
+   * By Ken Fyrstenberg
+   * http://stackoverflow.com/questions/21961839/simulation-background-size-cover-in-canvas
+   *
+   * drawImageProp(context, image [, x, y, width, height [,offsetX, offsetY]])
+   *
+   * If image and context are only arguments rectangle will equal canvas
+  */
+  function drawImageProp(ctx, img, x, y, w, h, offsetX, offsetY) {
+
+    if (arguments.length === 2) {
+        x = y = 0;
+        w = ctx.canvas.width;
+        h = ctx.canvas.height;
+    }
+
+    // default offset is center
+    offsetX = typeof offsetX === "number" ? offsetX : 0.5;
+    offsetY = typeof offsetY === "number" ? offsetY : 0.5;
+
+    // keep bounds [0.0, 1.0]
+    if (offsetX < 0) offsetX = 0;
+    if (offsetY < 0) offsetY = 0;
+    if (offsetX > 1) offsetX = 1;
+    if (offsetY > 1) offsetY = 1;
+
+    var iw = img.width,
+        ih = img.height,
+        r = Math.min(w / iw, h / ih),
+        nw = iw * r,   // new prop. width
+        nh = ih * r,   // new prop. height
+        cx, cy, cw, ch, ar = 1;
+
+    // decide which gap to fill    
+    if (nw < w) ar = w / nw;
+    if (nh < h) ar = h / nh;
+    nw *= ar;
+    nh *= ar;
+
+    // calc source rectangle
+    cw = iw / (nw / w);
+    ch = ih / (nh / h);
+
+    cx = (iw - cw) * offsetX;
+    cy = (ih - ch) * offsetY;
+
+    // make sure source rectangle is valid
+    if (cx < 0) cx = 0;
+    if (cy < 0) cy = 0;
+    if (cw > iw) cw = iw;
+    if (ch > ih) ch = ih;
+
+    // fill image in dest. rectangle
+    ctx.drawImage(img, cx, cy, cw, ch,  x, y, w, h);
+  }
+
   function mergeImageWithCanvas(imgAvatar, rgba, imgLogo, logoOpacity, size) {
     var canvas = document.getElementById('canvasProcessor');
     if (canvas.getContext) {
@@ -957,7 +1030,8 @@ return Dragdealer;
     var img = new Image();
     img.src = imgAvatar;
     img.onload = function() {
-      ctx.drawImage(img, 0, 0, size, size);
+      drawImageProp(ctx, img);
+      // ctx.drawImage(img, 0, 0, size, size);
       ctx.fillStyle = rgba;
       ctx.fillRect(0, 0, size, size);
 
@@ -974,7 +1048,7 @@ return Dragdealer;
   }
 
   function onConfirmButtonClick() {
-    mergeImageWithCanvas('../images/avatar.jpg', rgbaObjToString(rgba), '../images/alsogay.png', logoOpacity, 500);
+    mergeImageWithCanvas(avatarURL, rgbaObjToString(rgba), '../images/alsogay.png', logoOpacity, 500);
   }
 
   document.getElementById('confirmButton').onclick = onConfirmButtonClick;
